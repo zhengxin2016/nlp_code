070b88700709795deb811914abbc37d2734ebaf6
xin.zheng <xin.zheng@ecovacs.com>
2018-03-30 10:41:00 +0800

    fix

--- corpus/webservice/server_develop.py
+++ corpus/webservice/server_develop.py
@@ -187,7 +187,7 @@ def load_graph(scene):
         config = mongo_automata.load_graph_config(scene_id=scene)
         if not config:
             return {'result':'scene_id error'}
-        nodes = mongo.search('instruction', {'fields':['instruction', 'state_id']})
+        nodes = mongo.search('instruction', {'fields':['instruction', '_id']})
         edges = mongo.search('automata', {'fields':['intent']})
         #nodes = list(map(lambda x:x['instruction'], nodes))
         edges = list(map(lambda x:x['intent'], edges))
